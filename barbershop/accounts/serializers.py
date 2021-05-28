6d37d67c6d948402db8bddb4be6d1db4192ced1d
Irfan wani <irfanwani347@gmail.com>
2021-05-28 09:43:27 +0530

    added allow_blank=false to email field

--- barbershop/accounts/serializers.py
+++ barbershop/accounts/serializers.py
@@ -9,7 +9,7 @@ class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'email', 'password')
-        extra_kwargs = {'password': {'write_only': True}, 'email': {'required': True}}
+        extra_kwargs = {'password': {'write_only': True}, 'email': {'required': True, 'allow_blank': False}}
 
     def create(self, validated_data):
         user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
