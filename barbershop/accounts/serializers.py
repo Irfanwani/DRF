e15ff9f5350afc452f6c13556fb67529684499c5
Irfan wani <irfanwani347@gmail.com>
2021-05-28 12:17:27 +0530

    Added min_length to username and password

--- barbershop/accounts/serializers.py
+++ barbershop/accounts/serializers.py
@@ -9,7 +9,7 @@ class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'email', 'password')
-        extra_kwargs = {'password': {'write_only': True}, 'email': {'required': True, 'allow_blank': False}}
+        extra_kwargs = {'username': {'min_length': 3}, 'password': {'write_only': True, 'min_length': 8}, 'email': {'required': True, 'allow_blank': False}}
 
     def create(self, validated_data):
         user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
